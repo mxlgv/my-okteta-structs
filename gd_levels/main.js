7e4586f273adab6f68cb8564ee08b7af69c8d8a5
Maxim Logaev <maximlogaev2001ezro@gmail.com>
2023-08-21 23:33:36 +0300

    Added stripped TrackData

--- gd_levels/main.js
+++ gd_levels/main.js
@@ -15,27 +15,49 @@ You should have received a copy of the GNU General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 */
 
+
 function init() 
 {
-
-    const Track = struct({
-        Address: pointer(uint32(), uint8()),
-        Name:    string('ascii')
+    /*
+     * To get the coordinates of the start and finish of the track,
+     * you need: (value >> 16) << 3.
+    */
+    const TrackData = struct({
+        Separator:  uint8(), // Separates point data (0x33).
+        StartEncX:  int32(), // Encrypted coordinate X of start the track.
+        StartEncY:  int32(), // Encrypted coordinate Y of start the track.
+        FinishEncX: int32(), // Encrypted coordinate X of finish the track.
+        FinishEncY: int32(), // Encrypted coordinate Y of finish the track.
+        PointsNum:  uint16(),// The number of points in the track, not including the base ones.
+        BasePointX: int32(), // The base X coordinate from which all other points are calculated.
+        BasePointY: int32()  // The base Y coordinate from which all other points are calculated.
+        // DiffPoints:
+        /*
+         * An array of difference points. Like {x: uint8, y: uint8}.
+         * Point differences are calculated from the base point.
+         * The previous one is subtracted from the next one.
+        */
     });
 
-    const Difficulty = struct({
-        TracksNum:  uint32(),
+    const Track = struct({
+        Address: pointer(uint32(), TrackData),            // Address to TrackData.
+        Name:    string('ascii').set({terminatedBy : 0}), // Track name in ASCIIZ.
+    });
+
+    const Level = struct({
+        TracksNum:  uint32(), // Number of tracks in difficulty level.
         Tracks:     array(Track, function() { return this.parent.TracksNum.value; })
     });
 
-    var DifficultyLevels = struct({
-        LevelEasy:      Difficulty,
-        LevelMedium:    Difficulty,
-        LevelPro:       Difficulty
+    const Levels = struct({
+        LevelEasy:      Level,
+        LevelMedium:    Level,
+        LevelPro:       Level
     });
 
-    DifficultyLevels.byteOrder = "big-endian";
-    DifficultyLevels.name = "DifficultyLevels";
+    Levels.defaultLockOffset = 0x0;
+    Levels.byteOrder = "big-endian";
+    Levels.name = "Levels";
 
-    return DifficultyLevels;
+    return Levels;
 }
